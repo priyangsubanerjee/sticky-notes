dd2e8f7ae12987d58246f09886363fc88cf9db10
priyangsubanerjee <priyangsu26@gmail.com>
2022-12-13 01:28:05 +0530

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className='h-screen bg-neutral-100 overflow-auto'>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
