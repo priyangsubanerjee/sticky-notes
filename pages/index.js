4cc86695252b8bf0bedb74959a19aa93ebc6830b
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-12-17 19:24:20 +0530

    sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 
 import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
-import Head from "next/head";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
